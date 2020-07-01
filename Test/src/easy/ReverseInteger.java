b642850add9e5d13756e71e63e3a8743b5ae5791
lanitaleon <1912164282@qq.com>
2020-07-01 22:03:09 +0800

    reverse integer

--- Test/src/easy/ReverseInteger.java
+++ Test/src/easy/ReverseInteger.java
@@ -3,47 +3,68 @@ package easy;
 /**
  * 7
  * Given a 32-bit signed integer, reverse digits of an integer.
+ * example:
+ * Input: 123
+ * Output: 321
+ * Input: -123
+ * Output: -321
+ * Input: 120
+ * Output: 21
  */
 public class ReverseInteger {
-	
-	/*
-	 * 输入：123
-	 * 输出：321
-	 * 输入：-123
-	 * 输出：-321
-	 * 
-	 * 反向输出
-	 * The input is assumed to be a 32-bit signed integer. Your function should return 0 when the reversed integer overflows.
-	 * 溢出输出0
-	 * */
-	
-	public int reverse(int x) {
+    public int reverse(int x) {
+        if (x == 0) {
+            return x;
+        }
+        boolean negative = x < 0;
+        long mod = x % 10;
+        while (mod == 0) {
+            x = x / 10;
+            mod = x % 10;
+        }
+        String xStr = String.valueOf(x);
+        Long[] chars = new Long[xStr.length()];
+        for (int i = 0; i < xStr.length(); i++) {
+            long temp = Math.abs(x % 10);
+            chars[i] = temp;
+            x = x / 10;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            if (i == chars.length - 1 && chars[i] == 0) {
+                continue;
+            }
+            builder.append(chars[i]);
+        }
+        long d = Long.parseLong(builder.toString());
+        d = d < Integer.MIN_VALUE ? 0 : d;
+        d = d > Integer.MAX_VALUE ? 0 : d;
+        return negative ? (int) -d : (int) d;
+    }
 
-		//TODO 通过取余取整，将数字反向算出来，需要注意的是溢出的情况
-		int head = x / 10;
-		int tail = x % 10;
-		long re = 0;
+    /**
+     * best
+     *
+     * @param x number
+     * @return result
+     */
+    public int reverse2(int x) {
+        long result = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            result = result * 10 + pop;
+            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                return 0;
+            }
+            x /= 10;
+        }
+        return (int) result;
+    }
 
-		while (head != 0 || tail != 0) {
-			re = re * 10 + tail;
-			tail = head % 10;
-			head = head / 10;
-		}
-
-		re = re < Integer.MIN_VALUE ? 0 : re;
-		re = re > Integer.MAX_VALUE ? 0 : re;
-
-		return (int) re;
-
-		//TODO 转化为字符串操作，操作完成转回int型，溢出情况需要用double类型判断
-		//超时，一生之敌，我仿佛一个zz
-		/*
-		 * String s = x+""; String res = ""; if(s.charAt(0)=='-') { String str =
-		 * s.substring(1, s.length()); for(int i=0;i < str.length();i++){ str =
-		 * str.charAt(i)+str; } res = "-"+str; } else { for(int i=0;i <
-		 * s.length();i++){ s = s.charAt(i)+s; } res = s; } double num =
-		 * Double.valueOf(res); if(num < Integer.MAX_VALUE && num >
-		 * Integer.MIN_VALUE){ return (int)num; } else { return 0; }
-		 */
-	}
+    public static void main(String[] args) {
+        System.out.println(new ReverseInteger().reverse2(-2147483648));
+        System.out.println(new ReverseInteger().reverse2(123));
+        System.out.println(new ReverseInteger().reverse2(-123));
+        System.out.println(new ReverseInteger().reverse2(1202));
+    }
 }
