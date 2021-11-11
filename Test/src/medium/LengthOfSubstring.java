5ef1c58e169e86713bb556af66834d4f43bacaf4
lanitaleon <1912164282@qq.com>
2021-11-12 00:38:09 +0800

    重做 3

--- Test/src/medium/LengthOfSubstring.java
+++ Test/src/medium/LengthOfSubstring.java
@@ -1,43 +1,95 @@
 package medium;
 
+/**
+ * 3 无重复字符的最长子串
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
+ * 0 <= s.length <= 5 * 10^4
+ * s 由英文字母、数字、符号和空格组成
+ * <p>
+ * Examples:
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
 public class LengthOfSubstring {
-/*
-Given a string, find the length of the longest substring without repeating characters.
 
-Examples:
-
-Given "abcabcbb", the answer is "abc", which the length is 3.
-
-Given "bbbbb", the answer is "b", with the length of 1.
-
-Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
-Subscribe to see which companies asked this question
-*/
-	//the longest string must between two same char
-	//其他没有通过测试的算法
-	//ag1.循环嵌套四层，最容易想到的方法，从第一个字符开始循环查找
-	//ag2.ascii码查找，只考虑了abc的大写和小写，结果测试用例包含[]、‘这样的字符，这个方法是百度来的然而我找不到了
+    //the longest string must between two same char
+    //其他没有通过测试的算法
+    //ag1.循环嵌套四层，最容易想到的方法，从第一个字符开始循环查找
+    //ag2.ascii码查找，只考虑了abc的大写和小写，结果测试用例包含[]、‘这样的字符，这个方法是百度来的然而我找不到了
     static int[] last = new int[128];
-    public int lengthOfLongestSubstring(String s) {
-    //测试用例的字符从空格到大写字母Z，所以128足够了
+
+    /**
+     * 我写的 暴力
+     * 165ms 38.9 MB
+     */
+    public static int lengthOfLongestSubstring2(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int max = 1;
+        StringBuilder temp = new StringBuilder();
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (max >= s.length() - i) {
+                break;
+            }
+            temp.append(s.charAt(i));
+            for (int j = i + 1; j < s.length(); j++) {
+                if (temp.indexOf(String.valueOf(s.charAt(j))) == -1) {
+                    temp.append(s.charAt(j));
+                } else {
+                    max = Math.max(max, temp.length());
+                    temp = new StringBuilder();
+                    break;
+                }
+                if (j == s.length() - 1) {
+                    max = Math.max(max, temp.length());
+                    temp = new StringBuilder();
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("adbabcbb"));
+        System.out.println(lengthOfLongestSubstring("abc"));
+        System.out.println(lengthOfLongestSubstring("aaa"));
+        System.out.println(lengthOfLongestSubstring2("aab"));
+    }
+
+    /**
+     * 1ms 38.6 MB
+     * 16年提交的运行时间还是40ms左右，xs
+     * 感谢当时的我连注释都抄了
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        //测试用例的字符从空格到大写字母Z，所以128足够了
         int start = 0;
         int len = 0;
-        char[] w = new char[s.length()];
-        w = s.toCharArray();
-        for(int i = 0; i < 128; i++)
-            last[i] = -1;//last数组用于保存新出现的字符的下标，一开始全部初始化为-1
-        for(int i = 0; i < s.length(); ++ i){
-            if(last[w[i]-' '] >= start){ //当前这个字符出现过
-                if(i-start > len)
-                    len = i-start;
-                start = last[w[i]-' '] + 1; //从这个字符上次出现的位置+1，因为下一次要算长度的
-            }
-                last[w[i]-' '] = i;//更新当前字符的下标
+        char[] w = s.toCharArray();
+        for (int i = 0; i < 128; i++) {
+            //last数组用于保存新出现的字符的下标，一开始全部初始化为-1
+            last[i] = -1;
         }
-        if(len > s.length() - start)//针对结尾的字符没有在之前出现过，而结尾这个长度恰好是最长的情况，以及全部都是相同字符串的情况
-            return len;//比如aab这个字符串和bbbbb这个字符串
-        else
-            return s.length() - start;//一定要-start，因为有可能是“bbbbb”这种
+        for (int i = 0; i < s.length(); ++i) {
+            int currIndex = w[i] - ' ';
+//            int lastIndex = last[currIndex];
+            if (last[currIndex] >= start) {
+                //当前这个字符出现过
+                if (i - start > len) {
+                    len = i - start;
+                }
+                start = last[currIndex] + 1;
+                //从这个字符上次出现的位置+1，因为下一次要算长度的
+            }
+            last[currIndex] = i;//更新当前字符的下标
+        }
+        //针对结尾的字符没有在之前出现过，而结尾这个长度恰好是最长的情况，以及全部都是相同字符串的情况
+        //比如aab这个字符串和bbbbb这个字符串
+        //一定要-start，因为有可能是“bbbbb”这种
+        return Math.max(len, s.length() - start);
     }
 }
