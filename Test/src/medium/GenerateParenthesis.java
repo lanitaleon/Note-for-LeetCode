f8075057f6a9c67b36f7c6092d31bf97fc4ae776
lanitaleon <1912164282@qq.com>
2021-11-14 14:54:07 +0800

    新增 22

---
+++ Test/src/medium/GenerateParenthesis.java
@@ -0,0 +1,103 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 22 括号生成
+ * <p>
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * 有效括号组合需满足：左括号必须以正确的顺序闭合。
+ * <p>
+ * 1 <= n <= 8
+ */
+public class GenerateParenthesis {
+
+    /**
+     * 按括号序列的长度递归
+     * 9ms 38.4 MB
+     * https://leetcode-cn.com/problems/generate-parentheses/solution/gua-hao-sheng-cheng-by-leetcode-solution/
+     */
+    static List<String>[] cache = new List[100];
+
+    public static List<String> generateParenthesis3(int n) {
+        return generate(n);
+    }
+
+    public static List<String> generate(int n) {
+        if (cache[n] != null) {
+            return cache[n];
+        }
+        List<String> ans = new ArrayList<>();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c) {
+                for (String left : generate(c)) {
+                    for (String right : generate(n - 1 - c)) {
+                        ans.add("(" + left + ")" + right);
+                    }
+                }
+            }
+        }
+        cache[n] = ans;
+        return ans;
+    }
+
+    /**
+     * 回溯法 跟解法1实现基本一样
+     * 1ms 37.7 MB
+     * 关于结果中包含多少个元素可去了解 卡特兰数
+     */
+    public static List<String> generateParenthesis2(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public static void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+
+    /**
+     * 1ms 38.4 MB
+     * debug的感觉像二叉树 深度优先搜索
+     */
+    public static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        generate(res, "", 0, 0, n);
+        return res;
+    }
+
+    public static void generate(List<String> res, String ans, int count1, int count2, int n) {
+        if (count1 > n || count2 > n) {
+            return;
+        }
+        if (count1 == n && count2 == n) {
+            res.add(ans);
+        }
+        if (count1 >= count2) {
+            generate(res, ans + "(", count1 + 1, count2, n);
+            generate(res, ans + ")", count1, count2 + 1, n);
+        }
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(generateParenthesis(1));
+//        System.out.println(generateParenthesis2(2));
+        System.out.println(generateParenthesis3(2));
+    }
+}
