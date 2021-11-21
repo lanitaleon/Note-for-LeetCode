3a07764311402ff0ab378d6dd86c37a4f2495b8c
lanitaleon <1912164282@qq.com>
2021-11-21 16:35:09 +0800

    新增 148

---
+++ Test/src/medium/SortList.java
@@ -0,0 +1,191 @@
+package medium;
+
+import java.util.Arrays;
+
+/**
+ * 148 排序链表
+ * 给你链表的头结点head，请将其按 升序 排列并返回 排序后的链表 。
+ * <p>
+ * 链表中节点的数目在范围 [0, 5 * 10^4] 内
+ * -10^5 <= Node.val <= 10^5
+ * <p>
+ * 进阶：
+ * 你可以在O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+ */
+public class SortList {
+
+    /**
+     * 自底向上归并排序
+     * 9ms 46.8 MB
+     * https://leetcode-cn.com/problems/sort-list/solution/pai-xu-lian-biao-by-leetcode-solution/
+     */
+    public static ListNode sortList3(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        int length = 0;
+        ListNode node = head;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        ListNode dummyHead = new ListNode(0, head);
+        for (int subLength = 1; subLength < length; subLength <<= 1) {
+            ListNode prev = dummyHead, curr = dummyHead.next;
+            while (curr != null) {
+                ListNode head1 = curr;
+                for (int i = 1; i < subLength && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode head2 = curr.next;
+                curr.next = null;
+                curr = head2;
+                for (int i = 1; i < subLength && curr != null && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode next = null;
+                if (curr != null) {
+                    next = curr.next;
+                    curr.next = null;
+                }
+                prev.next = merge2(head1, head2);
+                while (prev.next != null) {
+                    prev = prev.next;
+                }
+                curr = next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+    public static ListNode merge2(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+
+    /**
+     * 自顶向下归并排序
+     * 5ms 46.8 MB
+     */
+    public static ListNode sortList2(ListNode head) {
+        return sortList(head, null);
+    }
+
+    public static ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        return merge(list1, list2);
+    }
+
+    public static ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+
+    /**
+     * 我写的
+     * 5ms 48.1 MB
+     */
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode countHead = head;
+        int count = 0;
+        while (countHead != null) {
+            count++;
+            countHead = countHead.next;
+        }
+        int[] nums = new int[count];
+        ListNode temp = head;
+        int index = 0;
+        while (temp != null) {
+            nums[index] = temp.val;
+            index++;
+            temp = temp.next;
+        }
+        Arrays.sort(nums);
+        ListNode res = new ListNode(nums[0]);
+        ListNode item = res;
+        for (int i = 1; i < nums.length; i++) {
+            item.next = new ListNode(nums[i]);
+            item = item.next;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+        ListNode res = sortList(head);
+        ListNode res2 = sortList2(head);
+        ListNode res3 = sortList3(head);
+        System.out.println(res.val);
+        System.out.println(res2.val);
+        System.out.println(res3.val);
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
