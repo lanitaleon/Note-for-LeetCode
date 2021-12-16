4a33f297caec5548c90bc7b48f27a29e4b9bff6a
lanitaleon <1912164282@qq.com>
2021-12-16 23:32:08 +0800

    新增 13

--- Test/src/easy/RomanToInteger.java
+++ Test/src/easy/RomanToInteger.java
@@ -4,9 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * roman to integer
- * description:
- * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * 13 罗马数字转整数
+ * 罗马数字包含一下七种字符: I, V, X, L, C, D, M.
  * I             1
  * V             5
  * X             10
@@ -14,37 +13,39 @@ import java.util.Map;
  * C             100
  * D             500
  * M             1000
- * For example, two is written as II in Roman numeral, just two one's added together.
- * Twelve is written as, XII, which is simply X + II.
- * The number twenty seven is written as XXVII, which is XX + V + II.
- * <p>
- * Roman numerals are usually written largest to smallest from left to right.
- * However, the numeral for four is not IIII. Instead, the number four is written as IV.
- * Because the one is before the five we subtract it making four.
- * The same principle applies to the number nine, which is written as IX.
- * There are six instances where subtraction is used:
- * <p>
- * I can be placed before V (5) and X (10) to make 4 and 9.
- * X can be placed before L (50) and C (100) to make 40 and 90.
- * C can be placed before D (500) and M (1000) to make 400 and 900.
- * Given a roman numeral, convert it to an integer.
- * Input is guaranteed to be within the range from 1 to 3999.
- * <p>
- * example:
- * Input: "III"
- * Output: 3
- * Input: "IV"
- * Output: 4
- * Input: "IX"
- * Output: 9
- * Input: "LVIII"
- * Output: 58
- * Explanation: L = 50, V= 5, III = 3.
- * Input: "MCMXCIV"
- * Output: 1994
- * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ * 例如， 罗马数字 2 写做II，即为两个并列的 1 。
+ * 12 写做XII，即为X+II。
+ * 27 写做XXVII, 即为XX+V+II。
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。
+ * 但也存在特例，例如 4 不写做IIII，而是IV。
+ * 数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。
+ * 同样地，数字 9 表示为IX。
+ * 这个特殊的规则只适用于以下六种情况：
+ * I可以放在V(5) 和X(10) 的左边，来表示 4 和 9。
+ * X可以放在L(50) 和C(100) 的左边，来表示 40 和90。
+ * C可以放在D(500) 和M(1000) 的左边，来表示400 和900。
+ * 给定一个罗马数字，将其转换成整数。
+ *
+ * 1 <= s.length <= 15
+ * s 仅含字符 ('I', 'V', 'X', 'L', 'C', 'D', 'M')
+ * 题目数据保证 s 是一个有效的罗马数字，且表示整数在范围 [1, 3999] 内
+ * 题目所给测试用例皆符合罗马数字书写规则，不会出现跨位等情况。
+ * IL 和 IM 这样的例子并不符合题目要求，49 应该写作 XLIX，999 应该写作 CMXCIX 。
  */
 public class RomanToInteger {
+
+    public static void main(String[] args) {
+        RomanToInteger rt = new RomanToInteger();
+        System.out.println(rt.romanToInt2("III"));
+        System.out.println(rt.romanToInt2("IV"));
+        System.out.println(rt.romanToInt2("IX"));
+        System.out.println(rt.romanToInt2("LVIII"));
+        System.out.println(rt.romanToInt("MCMXCIV"));
+    }
+
+    /**
+     * 5ms 38.9 MB
+     */
     public int romanToInt(String s) {
         Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
@@ -55,6 +56,11 @@ public class RomanToInteger {
         map.put('D', 500);
         map.put('M', 1000);
         int res = 0;
+        // 当前字符 >= 下一个字符 则累加
+        // 当前字符 < 下一个字符 则减去
+        // IV >> 1 < 5 >> 5-1=4
+        // XL >> 10 < 50 >> 50-10=40
+        // XI >> 10 > 1 >> 10+1=11
         for (int i = 0; i < s.length(); i++) {
             int n = map.get(s.charAt(i));
             if (i == s.length() - 1) {
@@ -73,9 +79,10 @@ public class RomanToInteger {
     }
 
     /**
-     * best
+     * 2ms 38.4 MB
      */
     public int romanToInt2(String s) {
+        // 思路和解法一是一样的 利用数组优化了速度
         int[] number = new int[s.length()];
         int i = 0;
         while (i < s.length()) {
@@ -112,13 +119,5 @@ public class RomanToInteger {
                 sum = sum + number[i];
         }
         return sum + number[number.length - 1];
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new RomanToInteger().romanToInt2("III"));
-        System.out.println(new RomanToInteger().romanToInt2("IV"));
-        System.out.println(new RomanToInteger().romanToInt2("IX"));
-        System.out.println(new RomanToInteger().romanToInt2("LVIII"));
-        System.out.println(new RomanToInteger().romanToInt2("MCMXCIV"));
     }
 }
