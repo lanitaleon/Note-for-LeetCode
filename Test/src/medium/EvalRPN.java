1a4296bd0b0732e65a8bdac5991f661491bb8695
lanitaleon <1912164282@qq.com>
2022-02-22 19:30:08 +0800

    新增 150

---
+++ Test/src/medium/EvalRPN.java
@@ -0,0 +1,140 @@
+package medium;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 150 逆波兰表达式求值
+ * 根据 逆波兰表示法，求表达式的值。
+ * 有效的算符包括 +、-、*、/ 。
+ * 每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+ * 注意 两个整数之间的除法只保留整数部分。
+ * 可以保证给定的逆波兰表达式总是有效的。
+ * 换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+ * <p>
+ * 提示：
+ * 1 <= tokens.length <= 10^4
+ * tokens[i] 是一个算符（"+"、"-"、"*" 或 "/"），或是在范围 [-200, 200] 内的一个整数
+ * <p>
+ * 逆波兰表达式：
+ * 逆波兰表达式是一种后缀表达式，所谓后缀就是指算符写在后面。
+ * 平常使用的算式则是一种中缀表达式，如 ( 1 + 2 ) * ( 3 + 4 ) 。
+ * 该算式的逆波兰表达式写法为 ( ( 1 2 + ) ( 3 4 + ) * ) 。
+ * 逆波兰表达式主要有以下两个优点：
+ * 去掉括号后表达式无歧义，上式即便写成 1 2 + 3 4 + * 也可以依据次序计算出正确结果。
+ * 适合用栈操作运算：遇到数字则入栈；遇到算符则取出栈顶两个数字进行计算，并将结果压入栈中
+ */
+public class EvalRPN {
+
+    public static void main(String[] args) {
+        EvalRPN er = new EvalRPN();
+        System.out.println(er.evalRPN2(new String[]{
+                "2", "1", "+", "3", "*"
+        }) == 9);
+        System.out.println(er.evalRPN3(new String[]{
+                "4", "13", "5", "/", "+"
+        }) == 6);
+        System.out.println(er.evalRPN(new String[]{
+                "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"
+        }) == 22);
+    }
+
+    /**
+     * 数组模拟栈
+     * 3ms 41.1 MB
+     * https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/solution/ni-bo-lan-biao-da-shi-qiu-zhi-by-leetcod-wue9/
+     */
+    public int evalRPN3(String[] tokens) {
+        int[] stack = new int[(tokens.length + 1) / 2];
+        int index = -1;
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    index--;
+                    stack[index] += stack[index + 1];
+                    break;
+                case "-":
+                    index--;
+                    stack[index] -= stack[index + 1];
+                    break;
+                case "*":
+                    index--;
+                    stack[index] *= stack[index + 1];
+                    break;
+                case "/":
+                    index--;
+                    stack[index] /= stack[index + 1];
+                    break;
+                default:
+                    index++;
+                    stack[index] = Integer.parseInt(token);
+            }
+        }
+        return stack[index];
+    }
+
+    /**
+     * LinkedList竟然比Stack快出1ms
+     * 5ms 40.9 MB
+     */
+    public int evalRPN2(String[] tokens) {
+        Deque<Integer> stack = new LinkedList<>();
+        for (String token : tokens) {
+            if (isNumber(token)) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(num1 + num2);
+                        break;
+                    case "-":
+                        stack.push(num1 - num2);
+                        break;
+                    case "*":
+                        stack.push(num1 * num2);
+                        break;
+                    case "/":
+                        stack.push(num1 / num2);
+                        break;
+                    default:
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public boolean isNumber(String token) {
+        return !("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token));
+    }
+
+    /**
+     * 我写的 栈
+     * 6ms 41.2 MB
+     */
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList<>();
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    int p1 = stack.pop();
+                    stack.push(stack.pop() - p1);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int d1 = stack.pop();
+                    stack.push(stack.pop() / d1);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
