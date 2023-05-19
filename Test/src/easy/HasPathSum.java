cd40b12f6bc531303bb4ee2eae6534ff9284052b
lanitaleon <1912164282@qq.com>
2023-05-19 20:41:37 +0800

    新增 112

---
+++ Test/src/easy/HasPathSum.java
@@ -0,0 +1,86 @@
+package easy;
+
+import bean.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+/**
+ * 112 路径总和
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。
+ * 判断该树中是否存在 根节点到叶子节点 的路径，
+ * 这条路径上所有节点值相加等于目标和 targetSum 。
+ * 如果存在，返回 true ；否则，返回 false 。
+ * 叶子节点 是指没有子节点的节点。
+ * 树中节点的数目在范围 [0, 5000] 内
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ */
+public class HasPathSum {
+    public static void main(String[] args) {
+        HasPathSum s = new HasPathSum();
+        TreeNode r = new TreeNode(5,
+                new TreeNode(4,
+                        new TreeNode(11,
+                                new TreeNode(7),
+                                new TreeNode(2)),
+                        null),
+                new TreeNode(8,
+                        new TreeNode(13),
+                        new TreeNode(4)));
+        System.out.println(s.hasPathSum2(r, 22));
+        System.out.println(s.hasPathSum(new TreeNode(1,
+                new TreeNode(2),
+                new TreeNode(3)), 5));
+        System.out.println(s.hasPathSum(null, 0));
+    }
+
+    /**
+     * bfs
+     * 1ms
+     */
+    public boolean hasPathSum2(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queNode = new LinkedList<>();
+        Queue<Integer> queVal = new LinkedList<>();
+        queNode.offer(root);
+        queVal.offer(root.val);
+        while (!queNode.isEmpty()) {
+            TreeNode now = queNode.poll();
+            int temp = Optional.ofNullable(queVal.poll()).orElse(0);
+            if (now.left == null && now.right == null) {
+                if (temp == sum) {
+                    return true;
+                }
+                continue;
+            }
+            if (now.left != null) {
+                queNode.offer(now.left);
+                queVal.offer(now.left.val + temp);
+            }
+            if (now.right != null) {
+                queNode.offer(now.right);
+                queVal.offer(now.right.val + temp);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 我写的 递归 dfs
+     * 0ms
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
+}
