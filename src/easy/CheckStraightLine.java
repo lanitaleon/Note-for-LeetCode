db5c0fb1d65d061ab70b05115bd7ff521e6b37d2
iris_spp <iris.song@derbysoft.net>
2025-08-04 14:52:09 +0800

    update 1232

--- src/easy/CheckStraightLine.java
+++ src/easy/CheckStraightLine.java
@@ -18,9 +18,26 @@ public class CheckStraightLine {
         System.out.println(!checkStraightLine.checkStraightLine(new int[][]{
                 {1, 1}, {2, 2}, {3, 4}, {4, 5}, {5, 6}, {7, 7}
         }));
-        System.out.println(checkStraightLine.checkStraightLine(new int[][]{
+        System.out.println(checkStraightLine.checkStraightLine2(new int[][]{
                 {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}
         }));
+    }
+
+    /**
+     * 0ms 民解 向量叉乘
+     */
+    public boolean checkStraightLine2(int[][] coordinates) {
+        int x1 = coordinates[1][0] - coordinates[0][0];
+        int y1 = coordinates[1][1] - coordinates[0][1];
+
+        for (int i = 2; i < coordinates.length; i++) {
+            int x2 = coordinates[i][0] - coordinates[0][0];
+            int y2 = coordinates[i][1] - coordinates[0][1];
+            if (x1 * y2 != x2 * y1) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
